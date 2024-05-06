cbd5452f5ca3e17020ac5e2c6206d153d599c531
Vinay Singh Chauhan <chauhansvinay@gmail.com>
2024-05-06 08:20:32 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+app.set('view engine', 'ejs')
+app.use(express.static(__dirname + '/public'));
+// blog home page
+app.get('/', (req, res) => {
+  res.render('pages/home');
+});
+
+app.get('/about', (req, res) => {
+  res.render('pages/about');
+});
+
+app.get('/application', (req, res) => {
+  res.render('pages/application');
+});
+app.get('/admissions', (req, res) => {
+  res.render('pages/admissions');
+});
+app.get('/fee', (req, res) => {
+  res.render('pages/fee');
+});
+app.get('/students-affair', (req, res) => {
+  res.render('pages/students-affair');
+});
+app.get('/ib', (req, res) => {
+  res.render('pages/ib');
+});
+app.get('/spa', (req, res) => {
+  res.render('pages/spa');
+});
+app.get('/course', (req, res) => {
+  res.render('pages/course');
+});
+app.get('/contact', (req, res) => {
+  res.render('pages/contact');
+});
+
+app.listen(5555)
+
+console.log('listening on port 5555')
